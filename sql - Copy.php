a720ff6b974c28400d93252441bb947dd3aa9eef
knelson22 <knelson22@gmail.com>
2017-05-12 21:40:54 -0500

    Add files via upload

---
+++ sql - Copy.php
@@ -0,0 +1,80 @@
+<?php
+
+
+
+
+    $servername = 'localhost';
+
+
+$username =
+$password = 
+$dbname = "karlscav_yp";
+
+
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error)
+{
+    die("Connection failed: " . $conn->connect_error);
+}
+
+query("set time_zone = '-6:00'");
+;
+;
+
+/*
+ * $sql = "SELECT * from zip";
+ * $result = $conn->query($sql);
+ *
+ * ;
+ *
+ */
+// var_dump($_SERVER);
+
+;
+;
+;
+
+function query($sql)
+{
+    global $conn;
+    if ($conn->query($sql) === TRUE)
+    {
+        //echo "New record created successfully";
+    }
+    else
+    {
+        //echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+    
+    ;
+}
+
+function updattotal($n, $i)
+{
+    $sql = "UPDATE `query`  set `total` = " . $n . " where `in` = " . $i;
+    query($sql);
+    
+    ;
+}
+
+function done($i)
+{
+    $sql = "UPDATE `query`  set `done` = 1  where `in` = " . $i;
+    query($sql);
+    
+    ;
+}
+
+function no_res($i)
+{
+    $sql = "UPDATE `query`  set `done` = 1,  `reason`   ='no-results-main' where `in` = " . $i;
+    query($sql);
+    
+    ;
+}
+
+?> 
